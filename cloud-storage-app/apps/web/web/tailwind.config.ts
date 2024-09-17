a3c3fa2306a7d3a762f035c455cec52a5bfef119
Khushi <you@example.com>
2024-09-17 17:21:07 +0530

    packages

--- cloud-storage-app/apps/web/web/tailwind.config.ts
+++ cloud-storage-app/apps/web/web/tailwind.config.ts
@@ -1,4 +1,14 @@
 import type { Config } from "tailwindcss";
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
 
 const config: Config = {
   content: [
